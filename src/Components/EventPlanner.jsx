fb193ee9339cd145d132005a3b06303dca456b30
etudiantxa <mormangane66@gmail.com>
2025-01-16 21:34:03 +0100

    message

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -4,11 +4,65 @@ import './EventPlanner.css'; // Import CSS file for styling
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
+        <header>
+            <h1>Bienvenue dans Event Planner</h1>
+        </header>
+        <section className="description">
+                <p>
+                    Planifiez et organisez vos événements sans effort avec Event Planner. Des anniversaires aux réunions d'entreprise, nous avons tout ce qu'il vous faut.
+                </p>
+
+
+                <button className="get-started-button">Commencer</button>
+        </section>
+        <section className="events_categories">
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday parties</li>
+                    <li>Anniversary celebrations</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby showers</li>
+                    <li>Graduation parties</li>
+                    <li>Family reunions</li>
+                </ul>
+                <ul>
+                    <h2> Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music festivals</li>
+                    <li>Film screenings</li>
+                    <li>Comedy shows</li>
+                    <li>Art exhibitions</li>
+                    <li>Cultural events</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising events</li>
+                    <li>Charity galas</li>
+                    <li>Volunteer drives</li>
+                    <li>Neighborhood block parties</li>
+                    <li>Community festivals</li>
+                    <li>Cultural celebrations</li>
+                </ul>
+        </section>
+        <section className="features">
+                <h2>Features</h2>
+                <ul>
+                    <li>Easy event creation and management</li>
+                    <li>Customizable event templates</li>
+                    <li>Guest list management</li>
+                    <li>Real-time collaboration</li>
+                    <li>Reminders and notifications</li>
+                </ul>
+        </section>
+        <section className="testimonials">
+
+        </section>
+        <section className="contact">
+
+        </section>
+    </div>
     );
 };
 
 export default EventPlanner;
+
